10aaf11039a078ea7652d61f1a1b468dbc5bac63
arjun23496 <arjun.k018@gmail.com>
2016-10-29 12:23:10 +0530

    Added credentials for twitter stream

--- data retrieval and preprocessing/tweets_crawler.py
+++ data retrieval and preprocessing/tweets_crawler.py
@@ -9,18 +9,15 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-#Variables that contains the user credentials to access Twitter API 
-access_token = "2718609962-z1P4vsKCHEJ8b0F6qZb2eHHCvAi8qN0c0E9lhqj"
-access_token_secret = "XiHiyPHTOq4plYV7WCx844BbbIPprt8kWWRboRElvEfrY"
-consumer_key = "ZDRMgVvy7ClZA0Jiu9vaMdlQo"
-consumer_secret = "hx0wqFNtMHgedhaoOZOVr9N33tHj1Nce3KHqJZXZzt2EZ57qQR"
+f = open('credentials/twitter.json')
+cred = json.loads(f.read())
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def __init__(self):
-        self.ctr = 24029
-        self.data_dir = "tweet\ crawler/"
+        self.ctr = 26802
+        self.data_dir = "tweet crawler/"
 
     def on_data(self, data):
         data = json.loads(data)
@@ -46,8 +43,8 @@ if __name__ == '__main__':
 
     #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = OAuthHandler(cred['consumer_key'], cred['consumer_secret'])
+    auth.set_access_token(cred['access_token'], cred['access_token_secret'])
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
